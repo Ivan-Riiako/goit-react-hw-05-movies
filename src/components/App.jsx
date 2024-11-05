14847d99a400da52b1bb1bba04c2a25c496bfa4a
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2024-11-05 19:30:11 +0200

    i'm back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 // import Cast from './Cast';
 // import Reviews from './Reviews';
 import Home from 'pages/Home';
-import MovieDetails from 'pages/MovieDetails';
 import Movies from 'pages/Movies';
+import MovieDetails from 'pages/MovieDetails';
 
 import { StyledLink, Navigation } from './App.styled';
 
