2da197d08fece1e85c3e6393c84796e7528fd9de
Ivan Riiako <52213412+Nogtbator@users.noreply.github.com>
2024-11-05 19:46:49 +0200

    05.11

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -38,6 +38,6 @@ export const StyledLink = styled(NavLink)`
 export const Navigation = styled.nav`
   display: flex;
   border-bottom: 3px solid gray;
-  /* box-shadow: 0px 1px 3px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 14%),
-    0px 2px 1px rgb(0 0 0 / 20%); */
+  box-shadow: 0px 1px 3px rgb(0 0 0 / 12%), 0px 1px 1px rgb(0 0 0 / 14%),
+    0px 2px 1px rgb(0 0 0 / 20%);
 `;
